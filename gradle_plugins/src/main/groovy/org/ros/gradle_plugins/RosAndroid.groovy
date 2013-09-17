025de846fa47833876c367ae396cd0e1caa9c019
Daniel Stonier <d.stonier@gmail.com>
2013-09-17 21:32:55 +0900

    added excludes to the ros android plugin.

--- gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosAndroid.groovy
+++ gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosAndroid.groovy
@@ -1,8 +1,8 @@
 package org.ros.gradle_plugins;
 
-import org.gradle.api.Project;
-import org.gradle.api.Plugin;
-import org.gradle.api.*;
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import java.util.HashMap
 
 /*
  * Configures java for the ros-android build environment. Pretty elementary right now,
@@ -16,8 +16,18 @@ class RosAndroidPlugin implements Plugin<Project> {
         if (!project.plugins.findPlugin('ros')) {
             project.apply(plugin: 'ros')
         }
+        project.apply(plugin: 'android')
         project.extensions.create("rosandroid", RosAndroidPluginExtension)
         project.rosandroid.buildToolsVersion = "17"
+        project.configurations.create('compile')
+        /* 
+         * Our maven repo 3rd parties are currently incompatible with android
+         * junit especially could use a look at - find a compatible version!
+         */
+        def excludes = new HashMap<String, String>()
+        excludes.put('group', 'junit')
+        excludes.put('group', 'xml-apis')
+        project.configurations['compile'].exclude(excludes)
     }
 }
 
