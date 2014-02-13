63b9a9c5879d151d4ce560f29edd9449597dc907
Damon Kohler <damonkohler@google.com>
2014-02-13 14:19:10 +0100

    Fix message dependency computation. Removes RosJavaPlugin in favor of having just the RosPlugin. Fixes several bugs related to reading paths form environment variables.

--- gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosPlugin.groovy
+++ gradle_plugins/src/main/groovy/org/ros/gradle_plugins/RosPlugin.groovy
@@ -1,8 +1,7 @@
 package org.ros.gradle_plugins;
 
-import org.gradle.api.Project;
-import org.gradle.api.Plugin;
 import org.gradle.api.*;
+import org.gradle.api.publish.maven.MavenPublication;
 
 /*
  * Provides information about the ros workspace.
@@ -11,56 +10,78 @@ import org.gradle.api.*;
  * - project.ros.mavenDeploymentRepository : location of the ros maven repository you will publish to
  *
  * It also performs the following actions
- * 
+ *
  * - checks and maeks sure the maven plugin is running
  * - constructs the sequence of dependant maven repos (local ros maven repos, mavenLocal, external ros maven repo)
  * - configures the uploadArchives for artifact deployment to the local ros maven repo (devel/share/maven)
  */
 class RosPlugin implements Plugin<Project> {
-    Project project
-    
-	def void apply(Project project) {
-	    this.project = project
-        if (!project.plugins.findPlugin('maven')) {
-            project.apply(plugin: 'maven')
-        }
-        /* Create project.ros.* property extensions */
-        project.extensions.create("ros", RosExtension)
-        project.ros.mavenPath = "$System.env.ROS_MAVEN_PATH".split(':')
-        project.ros.mavenDeploymentRepository = "$System.env.ROS_MAVEN_DEPLOYMENT_REPOSITORY"
-        def mavenRepository = "$System.env.ROS_MAVEN_REPOSITORY"
-        if ( mavenRepository != 'null' ) {
-            project.ros.mavenRepository = mavenRepository
-        }
-        /* 
-         * Could use some better handling for when this is not defined as it sets
-         * file://null, but it doesn't seem to hurt the process any
-         */
-        def repoURLs = project.ros.mavenPath.collect { 'file://' + it }
-        project.repositories {
-            repoURLs.each { p ->
-                maven {
-                    url p
-                }
-            }
-            mavenLocal()
-            maven {
-                url project.ros.mavenRepository
-            }
-            mavenCentral()
-        }
+  Project project
+
+  def void apply(Project project) {
+    this.project = project
+    project.apply plugin: "eclipse"
+    project.apply plugin: "java"
+    project.apply plugin: "maven"
+    project.apply plugin: "maven-publish"
+    project.apply plugin: "osgi"
+
+    project.sourceCompatibility = 1.6
+    project.targetCompatibility = 1.6
+
+    project.extensions.create("ros", RosPluginExtension)
+
+    project.ros.mavenRepository = System.getenv("ROS_MAVEN_REPOSITORY")
+    project.ros.mavenDeploymentRepository = System.getenv("ROS_MAVEN_DEPLOYMENT_REPOSITORY")
+
+    String mavenPath = System.getenv("ROS_MAVEN_PATH")
+    if (mavenPath != null) {
+      project.ros.mavenPath = mavenPath.tokenize(":")
     }
+
+    project.repositories {
+      if (project.ros.mavenPath != null) {
+        project.ros.mavenPath.each { path ->
+          maven {
+            url project.uri(path)
+          }
+        }
+      }
+      if (project.ros.mavenRepository != null) {
+        maven {
+          url project.ros.mavenRepository
+        }
+      }
+      mavenLocal()
+      maven {
+        url "http://repository.springsource.com/maven/bundles/release"
+      }
+      maven {
+        url "http://repository.springsource.com/maven/bundles/external"
+      }
+      mavenCentral()
+    }
+
+    if (project.ros.mavenDeploymentRepository != null &&
+        project.ros.mavenDeploymentRepository != "") {
+      project.publishing {
+        publications {
+          mavenJava(MavenPublication) {
+            from project.components.java
+          }
+        }
+        repositories {
+          maven {
+            url project.uri(project.ros.mavenDeploymentRepository)
+          }
+        }
+      }
+    }
+  }
 }
 
-/* http://www.gradle.org/docs/nightly/dsl/org.gradle.api.plugins.ExtensionAware.html */
-class RosExtension {
-    List<String> mavenPath
-    String mavenDeploymentRepository
-    String mavenRepository
-    
-    RosExtension() {
-        /* Initialising the strings here gets rid of the dynamic property deprecated warnings. */
-        this.mavenDeploymentRepository = ""
-        this.mavenRepository = "https://github.com/rosjava/rosjava_mvn_repo/raw/master"
-    }
+class RosPluginExtension {
+  String mavenRepository
+  String mavenDeploymentRepository
+  List<String> mavenPath
 }
